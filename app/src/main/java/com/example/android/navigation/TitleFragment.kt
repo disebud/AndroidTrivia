3e32a69db620036e31a1f2314d5a176ddf5b46ef
disebud <disebud@gmail.com>
2020-11-16 16:42:08 +0700

    First

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,40 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+    private lateinit var binding : FragmentTitleBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener { view: View ->
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+//        (activity as AppCompatActivity).supportActionBar?.title=getString(R.string.app_name)
+//        return inflater.inflate(R.layout.fragment_title, container, false)
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_options,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+                || NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+    }
+}
